32954016b5cc1a34d5c7d13a4043bec34a9c8502
Avi <avinash.kamath@ucdenver.edu>
2017-10-09 16:37:08 -0700

    updates from ResNet50 & Keras

---
+++ test.py
@@ -0,0 +1,10 @@
+def bubblesort(x):
+    for i in range(len(x)):
+        for j in range(len(x) - 1 - i):
+            if x[j] > x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
+            print(x)
+    print(x)
+
+
+print(bubblesort([2, 4, 1, 3, 7, 5, 4, 6, 9, 3, 1, 5]))
